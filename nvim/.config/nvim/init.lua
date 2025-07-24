7f1ebb3a607d5f08fae2ab95770c0b7ca2b04d9a
Shakir <shakir-redhead@outlook.com>
2025-07-24 10:47:09 +0100

    refactor: prep for stow, move configs to .config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -99,7 +99,7 @@ require('lazy').setup({
     'rmagatti/auto-session',
     opts = {
       log_level = 'error',
-      auto_session_enable_last_session = true,
+      auto_session_enable_last_session = false,
       auto_session_root_dir = vim.fn.stdpath 'data' .. '/sessions/',
       auto_session_enabled = true,
       auto_save_enabled = true,
