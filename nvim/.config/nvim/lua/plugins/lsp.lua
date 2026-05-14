e4c706dd52846d819cce9165da280119b532b507
Shakir <shakir-redhead@outlook.com>
2026-05-14 12:48:54 +0100

    feat(nvim): enable HTML language server

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -103,6 +103,9 @@ return {
       local capabilities = vim.tbl_deep_extend('force', vim.lsp.protocol.make_client_capabilities(), require('cmp_nvim_lsp').default_capabilities())
       -- LSP server configs
       local servers = {
+        html = {
+          filetypes = { 'html' },
+        },
         ts_ls = {},
         ruff = {},
         basedpyright = {
